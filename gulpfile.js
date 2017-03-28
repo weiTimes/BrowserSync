3d44b852a314dde13c84dece9e488ec7958cc413
mac <1156385527@qq.com>
2017-03-28 09:39:52 +0800

    修改了监听的文件目录

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ gulp.task('browser-sync', function() {
 		server: {
 			baseDir: './forest/' // 服务的根目录
 		},
-		files: ['forest/*.css', 'forest/*.html']
+		files: ['forest/**/*.css', 'forest/**/*.html']
 	});
 });
 
 gulp.task('babel', function() {
-	return gulp.src('forest/*.js')
+	return gulp.src('forest/**/*.js')
 		.pipe(babel())
 		.pipe(gulp.dest('forest/lib'));
 })
